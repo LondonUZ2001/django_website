fc19bc7600218e8cb8652a663695c6b3ff53fa29
LondonUZ2001 <alivooy13@gmail.com>
2020-07-02 19:21:13 +0100

    Added comment

--- polls/models.py
+++ polls/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
+    question_number = models.IntegerField(default=0)
     pub_date = models.DateTimeField('date published')
     def __str__(self):
         return self.question_text
--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,21 @@
 from django.http import HttpResponse
-
-
+from .models import Question
+from django.shortcuts import render
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+
+    return render(request,'polls/index.html')
+
+# Leave the rest of the views (detail, results, vote) unchanged
+
+
+    # Leave the rest of the views (detail, results, vote) unchanged
+def detail(request, question_id):
+    return render(request, 'polls/ali.html')
+
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
